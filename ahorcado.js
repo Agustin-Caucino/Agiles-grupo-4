e226ebab1951b54e18cfd5546ba662c7ee164162
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2025-06-02 21:37:44 -0300

    add funcionalidades necesarias sprint 2/6
    Co-authored-by: Agustin Caucino <Agustin-Caucino@users.noreply.github.com>
    Co-authored-by: Pedro Garrido <P-garrido@users.noreply.github.com>
    Co-authored-by: luciomondelli <luciomondelli@users.noreply.github.com>

--- ahorcado.js
+++ ahorcado.js
@@ -1,5 +1,45 @@
-function validarAdivinanza(palabra, adivinanza) {
-  return palabra === adivinanza;
+class Ahorcado {
+  constructor(palabra) {
+    this.palabra = palabra;
+    this.intentos = 6;
+    this.cantidadLetras = palabra.length;
+    this.cantidadLetrasDistintas = new Set(palabra).size;
+    this.letrasAdivinadas = [];
+    this.letrasIntentadas = [];
+    this.adivinanzaCorrecta = 'ejemplo';
+    this.adivinanzaIncorrecta = 'ejemlo';
+    this.intentosRestantes = this.intentos;
+    this.intentoFallido = 'x';
+    this.intentoCorrecto = 'e';
+  }
+
+  validarAdivinanza(palabra, adivinanza) {
+    return palabra === adivinanza;
+  }
+
+  intento(intento) {
+    if (this.letrasIntentadas.includes(intento)) {
+      return 'ya intentada';
+    }
+    this.letrasIntentadas.push(intento);
+    if (this.palabra.includes(intento)) {
+      this.letrasAdivinadas.push(intento);
+      return true;
+    } else {
+      this.intentosRestantes--;
+      return false;
+    }
+  }
+
+  resultado() {
+    if (this.intentosRestantes <= 0) {
+      return 'perdio';
+    } else if (this.letrasAdivinadas.length === this.cantidadLetrasDistintas) {
+      return 'gano';
+    } else {
+      return 'sigue jugando';
+    }
+  }
 }
 
-module.exports = { validarAdivinanza };
+module.exports = { Ahorcado };
--- ahorcado.spec.js
+++ ahorcado.spec.js
@@ -1,22 +1,57 @@
-const { validarAdivinanza } = require('./ahorcado.js');
+const { Ahorcado } = require('./ahorcado.js');
 
+let ahorcado = null;
+let letrasIncorrectas = [];
 describe('Funciones críticas para el ahorcado', () => {
   beforeEach(() => {
-    // Configuración inicial antes de cada prueba
-    this.palabra = 'ejemplo';
-    this.intentos = 6;
-    this.letrasAdivinadas = [];
-    this.adivinanzaCorrecta = 'ejemplo';
-    this.adivinanzaIncorrecta = 'ejemlo';
+    ahorcado = new Ahorcado('ejemplo');
+    letrasIncorrectas = ['x', 'y', 'z', 'q', 'w', 'r'];
   });
 
   it('debería confirmar el acierto en adivinar de la palabra', () => {
-    expect(validarAdivinanza(this.palabra, this.adivinanzaCorrecta)).toBe(true);
+    expect(
+      ahorcado.validarAdivinanza(ahorcado.palabra, ahorcado.adivinanzaCorrecta)
+    ).toBe(true);
   });
 
   it('debería confirmar el intento fallido de adivinar la palabra', () => {
-    expect(validarAdivinanza(this.palabra, this.adivinanzaIncorrecta)).toBe(
-      false
-    );
+    expect(
+      ahorcado.validarAdivinanza(
+        ahorcado.palabra,
+        ahorcado.adivinanzaIncorrecta
+      )
+    ).toBe(false);
+  });
+
+  it('debería confirmar el intento correcto de la letra', () => {
+    expect(ahorcado.intento(ahorcado.intentoCorrecto)).toBe(true);
+  });
+
+  it('debería confirmar el intento incorrecto de la letra', () => {
+    expect(ahorcado.intento(ahorcado.intentoFallido)).toBe(false);
+  });
+
+  it('debería confirmar que ganó', () => {
+    let letras = Array.from(new Set(ahorcado.palabra.split('')));
+    for (let letra of letras) {
+      ahorcado.intento(letra);
+    }
+    expect(ahorcado.resultado()).toEqual('gano');
+  });
+
+  it('debería confirmar que perdió', () => {
+    for (letra of letrasIncorrectas) {
+      ahorcado.intento(letra);
+    }
+    expect(ahorcado.resultado()).toEqual('perdio');
+  });
+
+  it('debería confirmar que sigue jugando', () => {
+    expect(ahorcado.resultado()).toEqual('sigue jugando');
+  });
+
+  it('debería no dejar adivinar una letra ya intentada', () => {
+    ahorcado.intento(ahorcado.intentoCorrecto);
+    expect(ahorcado.intento(ahorcado.intentoCorrecto)).toBe('ya intentada');
   });
 });
