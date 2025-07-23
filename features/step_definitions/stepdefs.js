684b6408884d7498798c96845536e751ba85fb21
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2025-07-23 10:27:57 -0300

    add waits to help tests

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -7,7 +7,7 @@ const chrome = require('selenium-webdriver/chrome');
 let ultimoResultado;
 let ultimaValidacion;
 const options = new chrome.Options();
-options.addArguments('--headless');
+// options.addArguments('--headless');
 options.addArguments('--no-sandbox');
 options.addArguments('--disable-dev-shm-usage');
 let driver = new Builder()
@@ -23,6 +23,7 @@ Given('que estoy en la pantalla principal', async function () {
 
 When('selecciono juego nuevo', async function () {
   await driver.findElement(By.id('start-game')).click();
+  await driver.sleep(500);
 });
 
 Then('debería crearse un nuevo juego', async function () {
@@ -32,14 +33,42 @@ Then('debería crearse un nuevo juego', async function () {
 });
 
 // Probando Letras
+// Given('que inició un juego', async function () {
+//   await driver.get('https://agustin-caucino.github.io/Agiles-grupo-4/');
+//   await driver.wait(until.elementLocated(By.id('start-game')));
+//   await driver.findElement(By.id('start-game')).click();
+// });
+
 Given('que inició un juego', async function () {
   await driver.get('https://agustin-caucino.github.io/Agiles-grupo-4/');
-  await driver.wait(until.elementLocated(By.id('start-game')));
-  await driver.findElement(By.id('start-game')).click();
+
+  // 1. Esperar a que la página se cargue completamente
+  await driver.wait(until.elementLocated(By.id('start-game')), 1000);
+
+  // 2. Obtener referencia fresca del elemento cada vez
+  const startButton = await driver.findElement(By.id('start-game'));
+
+  // 3. Esperar a que el elemento sea visible y clickeable
+  await driver.wait(until.elementIsVisible(startButton), 2000);
+  await driver.wait(until.elementIsEnabled(startButton), 2000);
+
+  // 4. Click con manejo de errores
+  try {
+    await startButton.click();
+  } catch (error) {
+    // Fallback: usar JavaScript click si el click normal falla
+    await driver.executeScript('arguments[0].click();', startButton);
+  }
+
+  // 5. Verificar que el juego realmente se inició
+  await driver.wait(until.elementLocated(By.id('word-display')), 1000);
+  const wordDisplay = await driver.findElement(By.id('word-display'));
+  await driver.wait(until.elementTextContains(wordDisplay, '_'), 1000);
 });
 
 When('ingreso una letra correcta', async function () {
   await driver.actions().sendKeys('a').perform();
+  await driver.sleep(500);
 });
 
 Then('debería ver que la letra pertenece a la palabra', async function () {
@@ -65,6 +94,7 @@ When('me quedo sin intentos', async function () {
   await driver.actions().sendKeys('w').perform();
   await driver.actions().sendKeys('x').perform();
   await driver.actions().sendKeys('b').perform();
+  await driver.sleep(500);
 });
 
 Then('debería ver mi resultado', async function () {
